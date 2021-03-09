e10b831d56a4969ec46e46bb70fa8ab6e54472af
Aljon Cruz <mindblendr@gmail.com>
2021-03-10 00:16:52 +0800

    Fixed factories

--- app/Models/Admin.php
+++ app/Models/Admin.php
@@ -7,11 +7,12 @@ use Laravel\Lumen\Auth\Authorizable;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Contracts\Auth\Authenticatable as AuthenticatableContract;
 use Illuminate\Contracts\Auth\Access\Authorizable as AuthorizableContract;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Tymon\JWTAuth\Contracts\JWTSubject;
 
 class Admin extends Model implements AuthenticatableContract, AuthorizableContract, JWTSubject
 {
-	use Authenticatable, Authorizable;
+	use Authenticatable, Authorizable, HasFactory;
 	protected $table = 'admin';
 	protected $guard = 'admin';
 
